a3a283a6541b9b71d6f17db55a0fc37d553d1f85
Roman <MontereyDSM@gmail.com>
2018-10-16 17:27:10 +0300

    styled

---
+++ components/Post/Post.stories.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+//
+// import Post from './Post';
+//
+// storiesOf('UserNavigation', module).add('default', () => <Post />);
