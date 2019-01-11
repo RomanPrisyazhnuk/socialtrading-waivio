18a949247695842c9971ac466a548e09e9ad0634
Roman <Waiviogit@gmail.com>
2019-01-11 12:14:19 +0200

    autolikes

--- components/Feed/Articles/Article/ModalArticle/ModalArticle.jsx
+++ components/Feed/Articles/Article/ModalArticle/ModalArticle.jsx
@@ -13,6 +13,7 @@ const propTypes = {
 const accounts = {
     stanislavh: { name: "stanislavh", postingKey: "5J9m5YLeCgiMK3duuLhJEsxyRNjEqmPm4o7aSXBvC7wLDAVWq9Q", defaultTags: 'spanish' },
     alexgrigurko: { name: "alexgrigurko", postingKey: "5JLry8DDdPAxRVoD3PVuEaXn8Fg8jGNCgEfVy36pd6s9sj4SixP", defaultTags: '' },
+    mersedes: { name: "alexgrigurko", postingKey: "5Kasb29ttWSdJqaCZV7bt19oA7Xn8XyHbiqAVmiLio8XNXDZX5j", defaultTags: '' },
     monterey: { name: "monterey", postingKey: "5JKTrjkXrXRsaYRwDpByazkjSm8juahvJLwjdVeRCXTAKFGMSU9", defaultTags: 'money trade news cryptocurrency economics' },
     waiviodev: { name: "waiviodev", postingKey: "5K5f6cnZGDmkugPNZNNYWQ9WZY9j9e5ykepmXK1TeFdY9nUB33Y", defaultTags: 'waivio' },
     eugenezh: { name: "eugenezh", postingKey: "5JEWK6YE4AcjAZDZt7xGKFPznKgijettZip6nWKmdvcsojsGfbG", defaultTags: '' },
